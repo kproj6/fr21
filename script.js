f011e99857710b398a2bfd4274dca99f696d15e9
Anders Smedegaard Pedersen <andespe@stud.ntnu.no>
2014-10-20 21:40:49 +0200

    wmts prototype

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ var map = new ol.Map({
     ],
     view: new ol.View({
       center: ol.proj.transform([8.8, 63.75], 'EPSG:4326', 'EPSG:3857'),
-      zoom:9 
+      zoom:9
     })
 }); 
-
 
 var imageLayer = new ol.layer.Image({
     opacity: 0.75,
@@ -29,6 +28,43 @@ var imageLayer = new ol.layer.Image({
 });
 
 map.addLayer(imageLayer);
+
+var projection = ol.proj.get('EPSG:3857');
+var projectionExtent = projection.getExtent();
+var size = ol.extent.getWidth(projectionExtent) / 256;
+var resolutions = new Array(14);
+var matrixIds = new Array(14);
+
+for (var z = 0; z < 14; ++z) {
+  resolutions[z] = size / Math.pow(2, z);
+  matrixIds[z] = z;
+}
+var attribution = new ol.Attribution({
+  html: 'Tiles &copy; <a href="http://services.arcgisonline.com/arcgis/rest/' +
+  'services/Demographics/USA_Population_Density/MapServer/">ArcGIS</a>'
+});
+
+var wmts = new ol.layer.Tile({
+  opacity: 0.7,
+    extent: projectionExtent,
+    source: new ol.source.WMTS({
+      attributions: [attribution],
+    url: 'http://services.arcgisonline.com/arcgis/rest/' +
+      'services/Demographics/USA_Population_Density/MapServer/WMTS/',
+    layer: '0',
+    matrixSet: 'EPSG:3857',
+    format: 'image/png',
+    projection: projection,
+    tileGrid: new ol.tilegrid.WMTS({
+      origin: ol.extent.getTopLeft(projectionExtent),
+    resolutions: resolutions,
+    matrixIds: matrixIds
+    }),
+    style: 'default'
+  })
+});
+
+map.addLayer(wmts);
 
 //bind checkbox to imageLayer visibility
 var visible = new ol.dom.Input(document.getElementById('visible'));
