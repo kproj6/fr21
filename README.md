157b04a9612be6f32e5ecc1035c35c626f0723ee
Anders Smedegaard Pedersen <andespe@stud.ntnu.no>
2014-10-23 12:25:41 +0200

    clean up and README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Functional requirement 2.1
-## Get a map up and running
+# Functional requirement 2.2
+## Show data for selected area
 
 this is a early prototype of the map user interface 
 
@@ -13,4 +13,23 @@ then run
 ```
   gulp
 ```
+
 to run a server on http://localhost:8080, and have the browser reload every time you make changes to a html, css or js file.
+
+to install icons install Bower
+```
+  npm install bower -g
+
+```
+then run
+
+```
+  bower install
+```
+in the repo directory
+
+
+## How to use the GUI
+write input in the ipunt fields and press submit to send a request to the backend
+
+To fill in coordinates you can hold shift and drag a box. This will fill in the coordinate values of the box. The values are not converted to the right projection at the moment
--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 'use scrict';
+var imageLayer; // global so we can removed it later
+var debth = document.getElementById('debth');
+var measure = document.getElementById('measure');
+var date = document.getElementById('date');
+var startCoord = document.getElementById('startCoord');
+var endCoord = document.getElementById('endCoord');
+var startCoordValue;
 
 function buildUrl(measure, startLat, startLon, endLat, endLon, depth, date){
    var url = 'http://178.62.233.73:10100/feature/' + measure +
@@ -26,19 +33,24 @@ var map = new ol.Map({
     })
 }); 
 
-var imageLayer = new ol.layer.Image({
+function updateImage(url){
+  if(map.getLayers().remove(imageLayer)){
+    map.getLayers().remove(imageLayer);
+  }
+  imageLayer = new ol.layer.Image({
     opacity: 0.75,
     source: new ol.source.ImageStatic({
-    url: 'http://178.62.233.73:10100/feature/salinity?startLat=65.24&startLon=7.56&endLat=65.42&endLon=9.542&depth=2&time=2013-08-05',
-    imageSize: [691, 541], // change to correct size
-    projection: map.getView().getProjection(),
-    imageExtent: ol.extent.applyTransform(
-      [8.12, 63.15, 8.9, 63.85], 
-      ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
+      url: url,
+      imageSize: [691, 541], // change to correct size
+      projection: map.getView().getProjection(),
+      imageExtent: ol.extent.applyTransform(
+        [8.12, 63.15, 8.9, 63.85], 
+        ol.proj.getTransform("EPSG:4326", "EPSG:3857"))
     })
-});
+  });
 
-map.addLayer(imageLayer);
+  map.addLayer(imageLayer);
+}
 
 //toggle custom Controls
 var toggleControls = document.getElementById('toggleControls');
@@ -53,14 +65,6 @@ toggleControls.addEventListener('click', function(){
 },false);
 
 //fr 2.2 stuff
-var debth = document.getElementById('debth');
-var measure = document.getElementById('measure');
-var date = document.getElementById('date');
-var startCoord = document.getElementById('startCoord');
-var endCoord = document.getElementById('endCoord');
-var startCoordValue;
-
-var dragBox; // global so we can remove it later
 
 function addDragBox() {
   dragBox = new ol.interaction.DragBox({
@@ -72,22 +76,25 @@ function addDragBox() {
     })
   });
   map.addInteraction(dragBox);
+  
+  dragBox.on('boxstart', function(evt){
+    startCoordValue = evt.coordinate;
+  });
+
+  dragBox.on('boxend', function(evt){
+    startCoord.value = startCoordValue.toString();
+    endCoord.value = evt.coordinate.toString();
+    var start = startCoord.value.split(',', 2);
+    var startLat = start[0]; 
+    var startLon = start[1]; 
+    var end = endCoord.value.split(',', 2);
+    var endLat = end[0];
+    var endLon = end[1];
+    //var url = 'http://178.62.233.73:10100/feature/salinity?startLat=65.24&startLon=7.56&endLat=65.42&endLon=9.542&depth=2&time=2013-08-05';
+    var url = buildUrl(measure.value, startLat, startLon, endLat, endLon, debth.value, date.value);
+    updateImage(url);
+    console.log(buildUrl(measure.value, startLat, startLon, endLat, endLon, debth.value, date.value));
+  });
 }
 
 addDragBox();
-
-dragBox.on('boxstart', function(evt){
-  startCoordValue = evt.coordinate;
-});
-
-dragBox.on('boxend', function(evt){
-  startCoord.value = startCoordValue.toString();
-  endCoord.value = evt.coordinate.toString();
-  var start = startCoord.value.split(',', 2);
-  var startLat = start[0]; 
-  var startLon = start[1]; 
-  var end = endCoord.value.split(',', 2);
-  var endLat = end[0];
-  var endLon = end[1];
-  console.log(buildUrl(measure.value, startLat, startLon, endLat, endLon, debth.value, date.value));
-});
