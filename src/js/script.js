7815318007b715365b9034e3a9b5337bde21c427
marcoradavelli <marcora@stud.ntnu.no>
2014-11-14 10:14:30 +0100

    checked image retrieval

--- src/js/script.js
+++ src/js/script.js
@@ -147,7 +147,7 @@ function addDragBox(conditionObj) {
     startCoord.value = proj4('EPSG:3785', 'WGS84', startCoordValue).reverse();
     endCoord.value = proj4('EPSG:3785', 'WGS84', evt.coordinate).reverse();
     console.log("After projecting to LatLong: "+startCoord.value+" - "+endCoord.value);
-
+/*
     proj4.defs("EPSG:9810", "+proj=stere +lat_ts=60 +lat_0=90 +lon_0=58 +k_0=1.0 +x_0=2412853.25 +y_0=1840933.25 +a=6370000 +b=6370000");
     var startPolarCoord = proj4('EPSG:3785', 'EPSG:9810', startCoordValue);
     var endPolarCoord = proj4('EPSG:3785', 'EPSG:9810', evt.coordinate);
@@ -158,7 +158,7 @@ function addDragBox(conditionObj) {
     var endPolarCoord = proj4('EPSG:4326', 'EPSG:9810', proj4('EPSG:3785', 'EPSG:4326', evt.coordinate));
     console.log("After projecting from WGS84 to PS-A: "+startPolarCoord+" - "+endPolarCoord);
     console.log("After projecting back again to WGS84: "+ proj4('EPSG:9810', 'EPSG:4326', startPolarCoord).reverse() + " - " + proj4('EPSG:9810', 'EPSG:4326', endPolarCoord).reverse() );
-
+*/
   });
 }
 
